50e5ef6a2de2abdae39cf7888a305d4b73531ebc
Catherine Wang <cw4@cs.wellesley.edu>
2022-11-16 14:18:45 -0500

    Added flask starter code to app.py

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
-written by rik
+'''
+CS304: GoodTimes Final Project
+Team: Audrey Liang, Rik Sampson, Catherine Wang
+Fall 2022
+'''
+
+from flask import (Flask, render_template, make_response, url_for, request,
+                   redirect, flash, session, send_from_directory, jsonify)
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+import cs304dbi as dbi
+import random
+
+app.secret_key = 'your secret here' # replace that with a random key
+app.secret_key = ''.join([ random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
+                                          'abcdefghijklmnopqrstuvxyz' +
+                                          '0123456789'))
+                           for i in range(20) ])
+
+# This gets us better error messages for certain common request errors
+app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+
+@app.route('/')
+def index():
+    return "Welcome to GoodTimes!"
+
+@app.before_first_request
+def init_db():
+    db_to_use = 'goodtime_db' # use team database
+    dbi.conf(db_to_use)
+    print('will connect to {}'.format(db_to_use))
+
+if __name__ == '__main__':
+    import sys, os
+    if len(sys.argv) > 1:
+        # arg, if any, is the desired port number
+        port = int(sys.argv[1])
+        assert(port>1024)
+    else:
+        port = os.getuid()
+    app.debug = True
+    app.run('0.0.0.0',port)
